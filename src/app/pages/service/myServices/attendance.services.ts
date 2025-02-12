010b5f69380fa24ec8b1223e399f616396329531
ishanchamika <ishanchami9@gmail.com>
2025-02-12 21:49:27 +0530

    get attendance data

--- src/app/pages/service/myServices/attendance.services.ts
+++ src/app/pages/service/myServices/attendance.services.ts
@@ -16,8 +16,14 @@ export class AttendanceService{
         return this.http.get(`${this.apiUrl}/Attendance`);
     }
 
+    getAttendanceByDateAndUserId(userId: string, date: string): Observable<any>
+    {
+        return this.http.post(`${this.apiUrl}/Attendance/getAllByUserIdAndDate`, { userId, date });
+    }
+
     markAttendance(userId: string, date: string, cameTime: string, leftTime: string, isUpdated: boolean, reason: string): Observable<any>
     {
         return this.http.post(`${this.apiUrl}/Attendance`, { userId, date, cameTime, leftTime, isUpdated, reason });
     }
+
 }
