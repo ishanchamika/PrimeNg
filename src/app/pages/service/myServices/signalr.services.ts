d526c5ef30e52d89475294cb922a35a2bdb57591
ishanchamika <ishanchami9@gmail.com>
2025-02-24 23:13:36 +0530

    signalr service

--- src/app/pages/service/myServices/signalr.services.ts
+++ src/app/pages/service/myServices/signalr.services.ts
@@ -1,51 +1,44 @@
 import { Injectable } from '@angular/core';
 import * as signalR from '@microsoft/signalr';
-import { sendMessage } from '@microsoft/signalr/dist/esm/Utils';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
-  })
+  providedIn: 'root',
+})
+export class SignalrService {
+  private hubConnection!: signalR.HubConnection;
+  private messagesSubject = new BehaviorSubject<{ username: string; msg: string }[]>([]);
+  messages$ = this.messagesSubject.asObservable();
 
-export class SignalrService
-{
-    private hubConnection!: signalR.HubConnection;
-    private messageSubject = new BehaviorSubject<string[]>([]);
-    messages$ = this.messageSubject.asObservable();
+  constructor() {}
 
-    constructor()
-    {
-        this.startConnection();
+  startConnection() {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:7248/Chat')
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    this.hubConnection.on('ReceiveSpecificMessage', (username: string, msg: string) => {
+      const currentMessages = this.messagesSubject.getValue();
+      this.messagesSubject.next([...currentMessages, { username, msg }]);
+    });
+
+    this.hubConnection.start()
+      .then(() => console.log('Connected to SignalR server!'))
+      .catch(err => console.error('Error connecting to SignalR:', err));
+  }
+
+  joinChatRoom(username: string, chatroom: string) {
+    if (this.hubConnection) {
+      this.hubConnection.invoke('JoinSpecificChatRoom', { username, chatroom })
+        .catch(err => console.error('Error joining chat room:', err));
     }
+  }
 
-    private startConnection() 
-    {
-        this.hubConnection = new signalR.HubConnectionBuilder()
-            .withUrl('http://localhost:7248/chat') 
-            .configureLogging(signalR.LogLevel.Information)
-            .withAutomaticReconnect()
-            .build();
-    
-        this.hubConnection.start()
-            .then(() => console.log('Connected to SignalR Hub'))
-            .catch(err => console.error('SignalR Connection Error:', err));
-    
-        this.hubConnection.on('ReceiveMessage', (username: string, message: string) => {
-            this.messageSubject.next([...this.messageSubject.value, `${username}: ${message}`]);
-        });
+  sendMessage(message: string) {
+    if (this.hubConnection) {
+      this.hubConnection.invoke('SendMessage', message)
+        .catch(err => console.error('Error sending message:', err));
     }
-    
-
-    joinChatRoom(username: string, chatRoom: string) {
-        const user = { username, chatRoom };
-        this.hubConnection.invoke('JoinSpecificChatRoom', user)
-            .catch(err => console.error('Join Room Error:', err));
-    }
-    
-
-    sendMessage(message: string, chatRoom: string) {
-        this.hubConnection.invoke('SendMessage', { message, chatRoom })
-            .catch(err => console.error('Send Message Error:', err));
-    }
-    
+  }
 }
