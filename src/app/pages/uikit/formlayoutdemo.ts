be67661981ffe76d583f87e331ab5495f9a89f97
ishanchamika <ishanchami9@gmail.com>
2025-02-26 22:02:26 +0530

    remove service part

--- src/app/pages/uikit/formlayoutdemo.ts
+++ src/app/pages/uikit/formlayoutdemo.ts
@@ -4,47 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import * as signalR from '@microsoft/signalr';
 import { BehaviorSubject } from 'rxjs';
+import { SignalrService } from '../service/myServices/signalr.services';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class SignalrService {
-  private hubConnection!: signalR.HubConnection;
-  private messagesSubject = new BehaviorSubject<string[]>([]);
-  messages$ = this.messagesSubject.asObservable();
-  private messages: string[] = [];
-
-  constructor(private http: HttpClient) {
-    this.startConnection();
-  }
-
-  private startConnection() {
-    this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl('https://localhost:7248/Chat')
-      .configureLogging(signalR.LogLevel.Information)
-      .build();
-
-    this.hubConnection
-      .start()
-      .then(() => console.log('Connected to SignalR'))
-      .catch(err => console.error('Error connecting to SignalR:', err));
-
-    this.hubConnection.on('ReceiveSpecificMessage', (username: string, msg: string) => {
-      this.messages.push(`${username}: ${msg}`);
-      this.messagesSubject.next([...this.messages]);
-    });
-  }
-
-  joinChatRoom(username: string, chatRoom: string) {
-    this.hubConnection.invoke('JoinSpecificChatRoom', { username, chatRoom })
-      .catch(err => console.error('Error joining chat room:', err));
-  }
-
-  sendMessage(message: string, chatRoom: string) {
-    this.hubConnection.invoke('SendMessage', message)
-      .catch(err => console.error('Error sending message:', err));
-  }
-}
 
 @Component({
   selector: 'app-chat',
